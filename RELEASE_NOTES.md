cf6a586082a309cc2c78e1cc4db9985737e1a1ba
Jeff Anderson <jeffa@unlocalhost.com>
2025-05-08 09:15:07 -0500

    try again
    updated:
      - one
      - two
      - three
    deleted:
      - this
      - and
      - that
    added:
      - foo
      - bar
      - baz

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -4,20 +4,8 @@ All notable changes to this project will be documented in this file.
 
 ## Unreleased
 
-- foo
-- bar
-- baz
 ### Added
--  (c756c8b)
 
-- one
-- two
-- three
 ### Updated
--  (c756c8b)
 
-- this
-- and
-- that
 ### Deleted
--  (c756c8b)
--- scripts/update_release_notes.sh
+++ scripts/update_release_notes.sh
@@ -79,7 +79,7 @@ while IFS= read -r -d '' hash && IFS= read -r -d '' body; do
     sed -i "/^${section_header}\$/a ${entry}" "$file"
     if [[ $rest != "$block" ]]; then
       while IFS= read -r sub; do
-        sed -i "/^${section_header}\$/i   ${sub}" "$file"
+        sed -i "/^${section_header}\$/a   ${sub}" "$file"
       done <<< "$rest"
     fi
   fi
