9735914c8b37cadbf375dc48791deadd7e23ea6f
Jeff Anderson <jeffa@unlocalhost.com>
2025-05-08 09:11:03 -0500

    try again
    updated:
      - one
      - two
      - three
    deleted:
      - this
      - and
      - that
    added:
      - foo
      - bar
      - baz

--- scripts/update_release_notes.sh
+++ scripts/update_release_notes.sh
@@ -49,7 +49,7 @@ while IFS= read -r -d '' hash && IFS= read -r -d '' body; do
 
       # bullet + SHA
       entry="- ${first_line} (${hash})"
-      sed -i "/^${section_header}\$/a ${entry}" "$file"
+      sed -i "/^${section_header}\$/i ${entry}" "$file"
 
       # any indented follow‐up lines
       if [[ $rest != "$block" ]]; then
@@ -76,7 +76,7 @@ while IFS= read -r -d '' hash && IFS= read -r -d '' body; do
     first_line="${block%%$'\n'*}"
     rest="${block#*$'\n'}"
     entry="- ${first_line} (${hash})"
-    sed -i "/^${section_header}\$/i ${entry}" "$file"
+    sed -i "/^${section_header}\$/a ${entry}" "$file"
     if [[ $rest != "$block" ]]; then
       while IFS= read -r sub; do
         sed -i "/^${section_header}\$/i   ${sub}" "$file"
